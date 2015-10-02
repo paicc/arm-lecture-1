dc1d313ac195ca6cb685310acb35131278486ff4
paicc <tony233534@gmail.com>
2015-10-02 18:16:13 +0800

    fib no recursion

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,22 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r3,#1       @L1=1
+	mov r4,#-1	@L2=-1
+	mov r5,#0	@total=0
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+loop:
+	add r5,r3,r4	@total=L1+L2
+	mov r4,r3	@L2=L1
+	mov r3,r5	@L1=total
+	subs r0,r0,#1	@x--
+	
+	cmp r0,#0	@while (x>=0)
+	bge loop
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	mov r0,r5  @retun value
+	pop {r3,r4,r5,pc}
+	
 	.size fibonacci, .-fibonacci
 	.end
